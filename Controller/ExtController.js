f6077d4794266b83664f3e183779c9b06ef78e19
rijul-kansal <kansalrijul@gmail.com>
2025-01-08 23:04:09 +0530

    adding external apis

--- Controller/ExtController.js
+++ Controller/ExtController.js
@@ -1,27 +1,53 @@
 const axios = require('axios');
 const ErrorClass = require('./../Utils/ErrorClass');
-const { messaging } = require('firebase-admin');
-const getState = async(req,res,next)=>{
+const getState = async (req, res, next) => {
+    try {
+        const country = req.body.country;
+        if (!country) {
+            return next(new ErrorClass('Please pass country', 400));
+        }
 
-    const country = req.body.country
-    if(!country)
-    {
-        return next(new ErrorClass('Please pass country',400));
-    }
-    const data  = await axios.post('https://countriesnow.space/api/v0.1/countries/states', {country})
-    console.log(data.data)
+        const { data } = await axios.post('https://countriesnow.space/api/v0.1/countries/states', { country });
+        const response = {
+            status: true,
+            data: data.data,
+        };
 
-    let response=
-    {
-        status:data.data.error,
-        data:{
-            data:data.data.data
+        res.status(200).json(response);
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            return next(new ErrorClass(err.response.data.msg,400));
+        }else
+        {
+            return next(new ErrorClass(err.message,400));
         }
     }
+};
 
-    res.status(200).json(response)
-}
+const getCity = async (req, res, next) => {
+    try {
+        const {country,state} = req.body;
+        if (!country || !state) {
+            return next(new ErrorClass('Please pass country or state', 400));
+        }
 
+        const { data } = await axios.post('https://countriesnow.space/api/v0.1/countries/state/cities', { country,state });
+        const response = {
+            status: true,
+            data: data.data,
+        };
+
+        res.status(200).json(response);
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            return next(new ErrorClass(err.response.data.msg,400));
+        }else
+        {
+            return next(new ErrorClass(err.message,400));
+        }
+    }
+};
 module.exports = {
-    getState
+    getState,
+    getCity
 }
--- Router/ExtRouter.js
+++ Router/ExtRouter.js
@@ -3,6 +3,7 @@ const Controller = require('./../Controller/ExtController')
 const router = express.Router()
 
 router.route('/getStates').post(Controller.getState)
+router.route('/getCity').post(Controller.getCity)
 
 module.exports = router
 
